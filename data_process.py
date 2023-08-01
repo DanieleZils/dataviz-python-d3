d40ef9242c56a14b33cc34288cab2a8192f84ec5
Daniele Lesniowski Zils <lesniowskidaniele@gmail.com>
2023-08-01 19:29:53 -0400

    more json data

--- data_process.py
+++ data_process.py
@@ -1,37 +1,48 @@
-import csv;
+import csv
+import json
+import datetime
+
+class JSONDateTimeEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, (datetime.date, datetime.datetime)):
+            return obj.isoformat()
+        else:
+            return json.JSONEncoder.default(self, obj)
+
+def dumps(obj):
+    return json.dumps(obj, cls=JSONDateTimeEncoder)
 
 nobel_winners = [
- {'category': 'Physics',
-  'name': 'Albert Einstein',
-  'nationality': 'Swiss',
-  'gender': 'male',
-  'year': 1921},
- {'category': 'Physics',
-  'name': 'Paul Dirac',
-  'nationality': 'British',
-  'gender': 'male',
-  'year': 1933},
- {'category': 'Chemistry',
-  'name': 'Marie Curie',
-  'nationality': 'Polish',
-  'gender': 'female',
-  'year': 1911}
+    {'category': 'Physics', 'name': 'Albert Einstein', 'nationality': 'Swiss', 'gender': 'male', 'year': 1921},
+    {'category': 'Physics', 'name': 'Paul Dirac', 'nationality': 'British', 'gender': 'male', 'year': 1933},
+    {'category': 'Chemistry', 'name': 'Marie Curie', 'nationality': 'Polish', 'gender': 'female', 'year': 1911}
 ]
 
-#create the CSV file and write the data to it
+# Create the CSV file and write the data to it
 cols = nobel_winners[0].keys()
 cols = sorted(cols)
 
 with open('data/nobel_winners.csv', 'w') as f:
-    fieldnames = nobel_winners[0].keys() 
-    fieldnames = sorted(fieldnames) 
+    fieldnames = nobel_winners[0].keys()
+    fieldnames = sorted(fieldnames)
     writer = csv.DictWriter(f, fieldnames=fieldnames)
-    writer.writeheader() 
+    writer.writeheader()
 
     for w in nobel_winners:
         writer.writerow(w)
 
-with open('data/nobel_winners.csv') as f:
-    reader = csv.reader(f)
-    for row in reader: 
-        print(row)
+# Convert 'year' to integers in nobel_winners list
+for w in nobel_winners:
+    w['year'] = int(w['year'])
+
+# Write the data to a JSON file using the dumps function
+with open('data/nobel_winners.json', 'w') as f:
+    json.dump(nobel_winners, f, default=JSONDateTimeEncoder)
+
+# Read the JSON file and load the data back to nobel_winners
+with open('data/nobel_winners.json') as f:
+    nobel_winners = json.load(f)
+
+
+
+
